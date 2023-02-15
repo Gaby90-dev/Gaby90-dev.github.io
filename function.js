ac152f9af0052fb9550203c19a261b177386af1b
Gaby90-dev <124525401+Gaby90-dev@users.noreply.github.com>
2023-02-15 20:19:49 -0300

    Delete function.js

--- function.js
+++
@@ -1,9 +0,0 @@
-function myFunction() {
-    document.getElementById("detalles").innerHTML = "Contacto: Liam Mckinney Teléfono: +54 11 5 213-2755";
-  }
-
-
-
-  function myFunction2() {
-    document.getElementById("detalles2").innerHTML = "Contacto: Daryl Owens Teléfono: +54 11 5 775-2571";
-  }  
